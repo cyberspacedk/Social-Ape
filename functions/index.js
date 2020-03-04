773690f8701bd9e7e4b543ef4c1856e484c74913
cyberspacedk <frontend.dk@gmail.com>
2020-03-04 23:03:09 +0200

    add express routing

--- functions/index.js
+++ functions/index.js
@@ -1,53 +1,49 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
 admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+const app = require("express")();
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
- });
-
-exports.getScreams = functions.https.onRequest((req, res)=>{
+app.get("/screams", (req, res) => {
   admin
-  .firestore()
-  .collection('screams')
-  .get()
-  .then(data=> {
-      let screams = []
-      data.forEach(doc=>{
-        screams.push(doc.data())
+    .firestore()
+    .collection("screams")
+    .orderBy("createdAt", "desc")
+    .get()
+    .then(data => {
+      let screams = [];
+      data.forEach(doc => {
+        screams.push({
+          screamId: doc.id,
+          body: doc.data().body,
+          userHandle: doc.data().userHandle,
+          createdAt: doc.data().createdAt
+        });
       });
       return res.json(screams);
     })
-  .catch(error=> console.log(error))
-})
+    .catch(error => console.log(error));
+});
 
-
-exports.createScream = functions.https.onRequest((req, res)=> {
-  if(req.method !== 'POST') {
-    res.status(400).json({error: 'Method not allowed'})
-  };
-  
+app.post("/scream", (req, res) => {
   const newScream = {
     body: req.body.body,
     userHandle: req.body.userHandle,
-    createdAt: admin.firestore.Timestamp.fromDate(new Date())
-  }
+    createdAt: new Date().toISOString()
+  };
 
   admin
-  .firestore()
-  .collection('screams')
-  .add(newScream)
-  .then(doc=> {
-    res.json({message: `document ${doc.id} created successfully`})
-  })
-  .catch(err=>{ 
-    res.status(500).json({error:'smth went wrong'});
-    console.log(err)
-  })
-})
+    .firestore()
+    .collection("screams")
+    .add(newScream)
+    .then(doc => {
+      res.json({ message: `document ${doc.id} created successfully` });
+    })
+    .catch(err => {
+      res.status(500).json({ error: "smth went wrong" });
+      console.log(err);
+    });
+});
 
+exports.api = functions.region("europe-west1").https.onRequest(app);
--- steps.md
+++ steps.md
@@ -1,6 +1,6 @@
 1. create new project in Firebase google console
-2. ```js $ npm install -g firebase-tools ```
-3. ```firebase login```
-4. ```firebase init```
+2. `js npm install -g firebase-tools`
+3. `firebase login`
+4. `firebase init`
 5. после инита описываем методы в папке functions
-6. ```firebase deploy```
+6. `firebase deploy`
