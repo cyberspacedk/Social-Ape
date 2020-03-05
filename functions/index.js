523d417d3e80b1bbeb41cfdcd43bd1e9febda137
cyberspacedk <frontend.dk@gmail.com>
2020-03-05 17:52:40 +0200

    create protect middleware

--- functions/index.js
+++ functions/index.js
@@ -119,10 +119,41 @@ app.get("/screams", (req, res) => {
     .catch(error => console.log(error));
 });
 
-app.post("/scream", (req, res) => {
+
+const authMiddleware = (req, res, next) => {
+  let idToken;
+
+  if(req.headers.authorization && req.headers.authorization.startsWith('Bearer ')){
+    idToken = req.headers.authorization.split('Bearer ')[1]
+  }else{
+    return res.status(403).json({error: 'Unauthorized'})
+  }
+
+  admin
+  .auth()
+  .verifyIdToken(idToken)
+  .then(decodedToken=>{
+    req.user = decodedToken;
+    return db
+    .collection('users')
+    .where('userId', '==', req.user.uid)
+    .limit(1)
+    .get()
+  })
+  .then(data=> {
+    req.user.handle = data.docs[0].data().handle;
+    return next()
+  })
+  .catch(err=>{
+    res.status(403).json(err)
+  })
+}
+
+
+app.post("/scream", authMiddleware, (req, res) => {
   const newScream = {
     body: req.body.body,
-    userHandle: req.body.userHandle,
+    userHandle: req.user.handle,
     createdAt: new Date().toISOString()
   };
 
--- functions/utils/helpers.js
+++ functions/utils/helpers.js
@@ -9,4 +9,33 @@ const isEmail = email => {
   return email.match(emailRegEx)  
 }
 
-module.exports = {isEmpty, isEmail}
+const authMiddleware = (req, res, next) => {
+  let idToken;
+
+  if(req.headers.authorization && req.headers.authorization.startsWith('Bearer ')){
+    idToken = req.headers.authorization.split('Bearer ')[1]
+  }else{
+    return res.status(403).json({error: 'Unauthorized'})
+  }
+
+  admin
+  .auth()
+  .verifyIdToken(idToken)
+  .then(decodedToken=>{
+    req.user = decodedToken;
+    return db
+    .collection('users')
+    .where('userId', '==', req.user.uid)
+    .limit(1)
+    .get()
+  })
+  .then(data=> {
+    req.user.handle = data.docs[0].data().handle;
+    return next()
+  })
+  .catch(err=>{
+    res.status(403).json(err)
+  })
+}
+
+module.exports = {isEmpty, isEmail, authMiddleware}
